fba32d8cd7260afb363fa9ff8f9662aec85c907b
fathanm10 <fathan.muhammad01@ui.ac.id>
2025-03-04 11:19:22 +0700

    add login form

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,9 @@ class RegisterForm(UserForm, FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')],
                                      render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Sign Up')
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)],
+                           render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[DataRequired()],
+                             render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
