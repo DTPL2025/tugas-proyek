c29c1be2a9ac531f954328a99ce3eeed1e967156
Amelia <mellistrue@gmail.com>
2025-03-16 21:38:20 +0800

    view buyer

--- app/routes.py
+++ app/routes.py
@@ -186,14 +186,14 @@ def view_product_buyer():
 
     return render_template('view_product_buyer.jinja', products=products)
 
-@app.route('/produk/list', methods=['GET'])
-def katalog_umkm():
-    products = Product.query.order_by(Product.name.asc()).all() 
-
-    return render_template('katalog_umkm.jinja', products=products)
-
 # @app.route('/produk/list', methods=['GET'])
-# def katalog_buyer():
+# def katalog_umkm():
 #     products = Product.query.order_by(Product.name.asc()).all() 
 
-#     return render_template('katalog_buyer.jinja', products=products)
+#     return render_template('katalog_umkm.jinja', products=products)
+
+@app.route('/produk/list', methods=['GET'])
+def katalog_buyer():
+    products = Product.query.order_by(Product.name.asc()).all() 
+
+    return render_template('katalog_buyer.jinja', products=products)
